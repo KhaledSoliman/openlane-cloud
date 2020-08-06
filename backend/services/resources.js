96e6c1b042de2c00378cc4189514354c24799b19
Khaled Soliman <khaledsoli111@aucegypt.edu>
2020-08-06 13:27:59 +0200

    notification disable

--- backend/services/resources.js
+++ backend/services/resources.js
@@ -22,21 +22,21 @@ class ResourceService {
     }
 
     async runJob(designName, regToken) {
-        const message = {
-            "notification": {
-                "title": "Jobs",
-                "body": "Your Job is now running"
-            },
-            token: regToken
-        };
-        admin.messaging().send(message)
-            .then((response) => {
-                // Response is a message ID string.
-                console.log('Successfully sent message:', response);
-            })
-            .catch((error) => {
-                console.log('Error sending message:', error);
-            });
+        // const message = {
+        //     "notification": {
+        //         "title": "Jobs",
+        //         "body": "Your Job is now running"
+        //     },
+        //     token: regToken
+        // };
+        // admin.messaging().send(message)
+        //     .then((response) => {
+        //         // Response is a message ID string.
+        //         console.log('Successfully sent message:', response);
+        //     })
+        //     .catch((error) => {
+        //         console.log('Error sending message:', error);
+        //     });
         logger.info("executing shell script...");
         const child = shell.exec(`sudo ./openlane-run.sh ${designName}`, {async: true});
         const self = this;

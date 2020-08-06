f6ac61fec91d2c243a8fd9fb787bb9a71e49ff05
Khaled Soliman <khaledsoli111@aucegypt.edu>
2020-08-06 13:36:49 +0200

    script update

--- backend/openlane-run.sh
+++ backend/openlane-run.sh
@@ -1,7 +1,8 @@
 cd openlane_working_dir/openlane
 # shellcheck disable=SC2046
+echo $PDK_ROOT
 DOCKER_ID=$(
   sudo docker run -div $(pwd):/openLANE_flow -v $PDK_ROOT:$PDK_ROOT -e PDK_ROOT=$PDK_ROOT -u $(id -u $USER):$(id -g $USER) openlane:rc2
 )
-docker exec "$DOCKER_ID" ./flow.tcl -design "$1"
-docker stop "$DOCKER_ID"
+sudo docker exec "$DOCKER_ID" ./flow.tcl -design "$1"
+sudo docker stop "$DOCKER_ID"

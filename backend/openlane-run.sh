fa4b614813b114c846bec2af10fbf44932015972
Khaled Soliman <khaledsoli111@aucegypt.edu>
2020-08-06 13:40:52 +0200

    script update

--- backend/openlane-run.sh
+++ backend/openlane-run.sh
@@ -1,5 +1,6 @@
 cd openlane_working_dir/openlane
 # shellcheck disable=SC2046
+PDK_ROOT=/home/khaledsoli111/openlane-cloud/backend/openlane_working_dir/pdks
 echo $PDK_ROOT
 DOCKER_ID=$(
   sudo docker run -div $(pwd):/openLANE_flow -v $PDK_ROOT:$PDK_ROOT -e PDK_ROOT=$PDK_ROOT -u $(id -u $USER):$(id -g $USER) openlane:rc2

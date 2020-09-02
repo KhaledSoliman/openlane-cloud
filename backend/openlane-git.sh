7aefa9f4bc121af8fb3450e0ca85be5879895270
Khaled Soliman <khaledsoli111@aucegypt.edu>
2020-09-02 14:42:06 +0200

    designName prefix

--- backend/openlane-git.sh
+++ backend/openlane-git.sh
@@ -1,3 +1,5 @@
+mkdir openlane_working_dir
+mkdir openlane_working_dir/pdks
 cd openlane_working_dir
 git clone https://github.com/efabless/openlane.git --branch develop
 cd openlane/docker_build
@@ -10,8 +12,16 @@ cd skywater-pdk
 git checkout 4e5e318e0cc578090e1ae7d6f2cb1ec99f363120
 git submodule update --init libraries/sky130_fd_sc_hd/latest
 make sky130_fd_sc_hd
+git submodule update --init libraries/sky130_fd_sc_hs/latest
+make sky130_fd_sc_hs
+git submodule update --init libraries/sky130_fd_sc_ms/latest
+make sky130_fd_sc_ms
+git submodule update --init libraries/sky130_fd_sc_ls/latest
+make sky130_fd_sc_ls
+git submodule update --init libraries/sky130_fd_sc_hdll/latest
+make sky130_fd_sc_hdll
 cd $PDK_ROOT
-git clone https://github.com/efabless/open_pdks.git -b develop
+git clone https://github.com/efabless/open_pdks.git -b rc2
 cd open_pdks
 make
 make install-local

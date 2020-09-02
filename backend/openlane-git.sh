2ad8b978036643f2ab345ae2e4170c411b6158b3
Khaled Soliman <khaledsoli111@aucegypt.edu>
2020-09-02 13:34:26 +0200

    git script update

--- backend/openlane-git.sh
+++ backend/openlane-git.sh
@@ -1,17 +1,17 @@
 cd openlane_working_dir
-git clone git@github.com:efabless/openlane --branch develop
+git clone https://github.com/efabless/openlane.git --branch develop
 cd openlane/docker_build
 make merge
 cd ..
-export PDK_ROOT=/Users/khaled/WebstormProjects/openlane-cloud/backend/openlane_working_dir/pdks
+export PDK_ROOT=/home/ks/openlane-cloud/backend/openlane_working_dir/pdks
 cd  $PDK_ROOT
-git clone git@github.com:google/skywater-pdk.git
+git clone https://github.com/google/skywater-pdk.git
 cd skywater-pdk
 git checkout 4e5e318e0cc578090e1ae7d6f2cb1ec99f363120
 git submodule update --init libraries/sky130_fd_sc_hd/latest
 make sky130_fd_sc_hd
 cd $PDK_ROOT
-git clone git@github.com:efabless/open_pdks.git -b develop
+git clone https://github.com/efabless/open_pdks.git -b develop
 cd open_pdks
 make
 make install-local

92960b949506189d3417520f671326ac5a84aec7
Khaled Soliman <khaledsoli111@aucegypt.edu>
2020-09-02 18:49:06 +0200

    debugging

--- backend/openlane-git.sh
+++ backend/openlane-git.sh
@@ -1,32 +1,11 @@
 mkdir openlane_working_dir
 mkdir openlane_working_dir/pdks
+export PDK_ROOT=/home/ks/openlane-cloud/backend/openlane_working_dir/pdk
+export IMAGE_NAME=openlane:cloud
 cd openlane_working_dir
 git clone https://github.com/efabless/openlane.git --branch develop
-cd openlane/docker_build
-make merge
-cd ..
-export PDK_ROOT=/home/ks/openlane-cloud/backend/openlane_working_dir/pdks
-cd  $PDK_ROOT
-git clone https://github.com/google/skywater-pdk.git
-cd skywater-pdk
-git checkout 3f310bcc264df0194b9f7e65b83c59759bb27480
-git submodule update --init libraries/sky130_fd_sc_hd/latest
-make sky130_fd_sc_hd
-git submodule update --init libraries/sky130_fd_sc_hs/latest
-make sky130_fd_sc_hs
-git submodule update --init libraries/sky130_fd_sc_ms/latest
-make sky130_fd_sc_ms
-git submodule update --init libraries/sky130_fd_sc_ls/latest
-make sky130_fd_sc_ls
-git submodule update --init libraries/sky130_fd_sc_hdll/latest
-make sky130_fd_sc_hdll
-cd $PDK_ROOT
-git clone https://github.com/efabless/open_pdks.git -b rc2
-cd open_pdks
-git checkout 52f78fa08f91503e0cff238979db4589e6187fdf
-make
-make install-local
-./configure --with-sky130-source=$PDK_ROOT/skywater-pdk/libraries --with-sky130-local-path=$PDK_ROOT && \
-	cd sky130
-	make
-	make install-local
+cd openlane
+make clone-skywater-pdk
+make all-skywater-libraries
+make open_pdks
+make openlane
--- backend/openlane-run.sh
+++ backend/openlane-run.sh
@@ -1,7 +1,7 @@
 cd openlane_working_dir/openlane
 # shellcheck disable=SC2046
-PDK_ROOT=/home/khaledsoli111/openlane-cloud/backend/openlane_working_dir/pdks
-DOCKER_ID=$(sudo docker run --rm --name="$3" -div $(pwd):/openLANE_flow -v $PDK_ROOT:$PDK_ROOT -e PDK_ROOT=$PDK_ROOT -u $(id -u $USER):$(id -g $USER) openlane:rc2)
+PDK_ROOT=/home/ks/openlane-cloud/backend/openlane_working_dir/pdks
+DOCKER_ID=$(sudo docker run --rm --name="$3" -div $(pwd):/openLANE_flow -v $PDK_ROOT:$PDK_ROOT -e PDK_ROOT=$PDK_ROOT -u $(id -u $USER):$(id -g $USER) openlane:cloud)
 case "$1" in
 normal)
   sudo docker exec "$DOCKER_ID" python3 run_designs.py --designs "$2" --tag "$3" --threads 1 --clean

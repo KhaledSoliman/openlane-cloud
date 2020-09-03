10e2c0b381df5597c32e78c5ccad6cc1dd998e29
Khaled Soliman <khaledsoli111@aucegypt.edu>
2020-09-03 15:58:30 +0200

    debugging

--- backend/openlane-git.sh
+++ backend/openlane-git.sh
@@ -1,11 +1,11 @@
 mkdir openlane_working_dir
 mkdir openlane_working_dir/pdks
-export PDK_ROOT=/home/ks/openlane-cloud/backend/openlane_working_dir/pdk
+export PDK_ROOT=/home/ks/openlane-cloud/backend/openlane_working_dir/pdks
 export IMAGE_NAME=openlane:cloud
 cd openlane_working_dir
 git clone https://github.com/efabless/openlane.git --branch develop
 cd openlane
+make openlane
 make clone-skywater-pdk
 make all-skywater-libraries
 make open_pdks
-make openlane
--- backend/services/git.js
+++ backend/services/git.js
@@ -11,10 +11,10 @@ class Git {
     async cloneRepo(repoURL, jobId, designName) {
         logger.info(`Cloning repository: ${repoURL}`);
         await shell.exec(`git clone ${repoURL} ${this.reposPath}/${jobId}-${designName}`);
-        const data = fs.readFileSync(`${this.reposPath}/${jobId}-${designName}/config.tcl`, 'utf-8');
-        let newValue = data.replace(/^(set ::env\(DESIGN_NAME\) ").+(")/gm, `$1${jobId}-${designName}$2`);
-        newValue = newValue.replace(/^(set ::env\(VERILOG_FILES\) \[glob \$::env\(OPENLANE_ROOT\)\/designs\/).+(\/src\/\*\.v])$/gm, `$1${jobId}-${designName}$2`);
-        fs.writeFileSync(`${this.reposPath}/${jobId}-${designName}/config.tcl`, newValue, 'utf-8');
+        // const data = fs.readFileSync(`${this.reposPath}/${jobId}-${designName}/config.tcl`, 'utf-8');
+        // let newValue = data.replace(/^(set ::env\(DESIGN_NAME\) ").+(")/gm, `$1${jobId}-${designName}$2`);
+        // newValue = newValue.replace(/^(set ::env\(VERILOG_FILES\) \[glob \$::env\(OPENLANE_ROOT\)\/designs\/).+(\/src\/\*\.v])$/gm, `$1${jobId}-${designName}$2`);
+        // fs.writeFileSync(`${this.reposPath}/${jobId}-${designName}/config.tcl`, newValue, 'utf-8');
     }
 
     async deleteRepo(jobId, designName) {
